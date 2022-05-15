d11e08daa39a9910eda7bbb9d085da98ca8a9a90
Ciro Duran <ciro.duran@bossalien.com>
2022-05-15 23:21:43 +0100

    Increase polling times to reduce server load

--- frontend/BossAlienMediaPlayer/src/environments/environment.prod.ts
+++ frontend/BossAlienMediaPlayer/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  nowPlayingPollTime : 1000,
-  queuePollTime : 1000,
+  nowPlayingPollTime : 2000,
+  queuePollTime : 3000,
   historyPollTime : 60000,
   queueUpdatePauseDuration: 2000,
   searchDebounceTime: 600,
